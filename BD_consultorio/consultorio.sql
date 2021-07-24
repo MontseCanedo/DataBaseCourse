ee6ebddb9a476cc5d753eec824dee0a65840db1b
Montserrat Canedo <vmontse_cgutierrez@hotmail.com>
2021-07-24 12:23:28 -0400

    creacion base de datos consultorio

--- BD_consultorio/consultorio.sql
+++ BD_consultorio/consultorio.sql
@@ -86,3 +86,39 @@ add constraint `analisispacientes_analisis`
 `fecha` date,
 `precio` int not null
 );
+
+ALTER TABLE `consultorio`.`historia_clinica` 
+CHARACTER SET = utf8 ,
+ADD INDEX `id_paciente_idx` (`id_paciente` ASC) VISIBLE;
+;
+ALTER TABLE `consultorio`.`historia_clinica` 
+ADD CONSTRAINT `id_paciente`
+  FOREIGN KEY (`id_paciente`)
+  REFERENCES `consultorio`.`pacientes` (`id`)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
+
+create table `consultorio`.`historiaClinica_tratamiento`(
+`id_tratamiento` int not null,
+`id_historiaClinica` int not null
+);
+
+alter table `consultorio`.`historiaClinica_tratamiento`
+add index `historiaClinicatratamiento_historia_clinica_idx` (`id_historiaClinica` asc);
+;
+alter table `consultorio`.`historiaClinica_tratamiento`
+add constraint `historiaClinicatratamiento_historia_clinica`
+    foreign key (`id_historiaClinica`)
+    references `consultorio`.`historia_clinica` (`id_paciente`)
+    on delete cascade
+    on update cascade;
+
+alter table `consultorio`.`historiaClinica_tratamiento`
+add index `historiaClinicatratamiento_tratamiento_idx` (`id_tratamiento` asc);
+;
+alter table `consultorio`.`historiaClinica_tratamiento`
+add constraint `historiaClinicatratamiento_tratamiento`
+    foreign key (`id_tratamiento`)
+    references `consultorio`.`tratamiento` (`id_tratamiento`)
+    on delete cascade
+    on update cascade;
